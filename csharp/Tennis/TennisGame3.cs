1fe73842b8ddc9724639b7cdd911686c18ca0079
riccardo <dellasantar@gmail.com>
2018-01-27 11:29:58 +0100

    simple observer pattern

--- csharp/Tennis/TennisGame3.cs
+++ csharp/Tennis/TennisGame3.cs
@@ -2,117 +2,135 @@ using System;
 
 namespace Tennis
 {
-    public class Player
+
+    //public interface IResultScoreCalculator
+    //{
+    //    string GetScore(ObservablePlayer p1, ObservablePlayer p2);
+    //}
+
+    //public class ResultScoreCalculator : IResultScoreCalculator
+    //{
+    //    private string[] PointNames = { "Love", "Fifteen", "Thirty", "Forty" };
+    //    public string GetScore(ObservablePlayer p1, ObservablePlayer p2)
+    //    {
+    //        if ((p1.GetCurrentScore() < 4 && p2.GetCurrentScore() < 4)
+    //             && (p1.GetCurrentScore() + p2.GetCurrentScore() < 6))
+    //        {
+    //            return GetPartialScore(p1, p2);
+    //        }
+    //        else
+    //        {
+    //            return GetFinalScore(p1, p2);
+
+    //        }
+    //    }
+    //    private string GetFinalScore(ObservablePlayer p1, ObservablePlayer p2)
+    //    {
+    //        if (p1.GetCurrentScore() == p2.GetCurrentScore())
+    //            return "Deuce";
+    //        string retValue = p1.GetCurrentScore() > p2.GetCurrentScore() ? p1.Name : p2.Name;
+    //        return (1 == Math.Abs(p1.GetCurrentScore() - p2.GetCurrentScore())) ? "Advantage " + retValue : "Win for " + retValue;
+    //    }
+
+    //    private string GetPartialScore(ObservablePlayer p1, ObservablePlayer p2)
+    //    {
+    //        string retValue = PointNames[p1.GetCurrentScore()];
+    //        return (p1.GetCurrentScore() == p2.GetCurrentScore()) ? retValue + "-All" : retValue + "-" + PointNames[p2.GetCurrentScore()];
+    //    }
+    //}
+
+    public class ObservablePlayer
     {
         public string Name { get; set; }
 
-        public EventHandler PlayerScored;
+        public EventHandler Player1Scored;
+        public EventHandler Player2Scored;
 
-        public Player(string name)
+        public ObservablePlayer(string name)
         {
             this.Name = name;
         }
 
-        public void WonPoint()
+        public void Player1ScoredOnePoint()
         {
-            EventHandler handler = PlayerScored;
+            EventHandler handler = Player1Scored;
             
             if (handler != null)
-            {
                 handler(this, EventArgs.Empty);
-            }
+            
+        }
+        public void Player2ScoredOnePoint()
+        {
+            EventHandler handler = Player2Scored;
+
+            if (handler != null)
+                handler(this, EventArgs.Empty);
+            
         }
 
-        public int GetCurrentScore()
-        {
-            return _Score;
-        }
+
+
+        //public int GetCurrentScore()
+        //{
+        //    return _Score;
+        //}
 
     }
-    public interface IResultScoreCalculator
-    {
-        string GetScore(Player p1, Player p2);
-    }
-
-    public class ResultScoreCalculator : IResultScoreCalculator
-    {
-        private string[] PointNames = { "Love", "Fifteen", "Thirty", "Forty" };
-        public string GetScore(Player p1, Player p2)
-        {
-            if ((p1.GetCurrentScore() < 4 && p2.GetCurrentScore() < 4)
-                 && (p1.GetCurrentScore() + p2.GetCurrentScore() < 6))
-            {
-                return GetPartialScore(p1, p2);
-            }
-            else
-            {
-                return GetFinalScore(p1, p2);
-
-            }
-        }
-        private string GetFinalScore(Player p1, Player p2)
-        {
-            if (p1.GetCurrentScore() == p2.GetCurrentScore())
-                return "Deuce";
-            string retValue = p1.GetCurrentScore() > p2.GetCurrentScore() ? p1.Name : p2.Name;
-            return (1==Math.Abs(p1.GetCurrentScore() - p2.GetCurrentScore())) ? "Advantage " + retValue : "Win for " + retValue;
-        }
-
-        private string GetPartialScore(Player p1, Player p2)
-        {
-            string retValue = PointNames[p1.GetCurrentScore()];
-            return (p1.GetCurrentScore() == p2.GetCurrentScore()) ? retValue + "-All" : retValue + "-" + PointNames[p2.GetCurrentScore()];
-        }
-    }
+ 
 
     class ObserverTabellone
     {
         private int scoreP1 { get; set; }
         private int scoreP2 { get; set; }
 
-        private IResultScoreCalculator rsc;
+        //private IResultScoreCalculator rsc;
 
         public ObserverTabellone()
         {
-            rsc = new ResultScoreCalculator();
+            //rsc = new ResultScoreCalculator();
             scoreP1 = 0;
             scoreP2 = 0;
         }
-        public void HandleEvent(object sender, EventArgs args)
+
+        public void Player1ScoredOnePoint(object sender, EventArgs args)
         {
-            Console.WriteLine("Something happened to " + sender);
+            scoreP1++;
+        }
+        public void Player2ScoredOnePoint(object sender, EventArgs args)
+        {
+            scoreP2++;
         }
     }
 
     public class TennisGame3 : ITennisGame
     {
-        private Player p2;
-        private Player p1;
+        private ObservablePlayer p2;
+        private ObservablePlayer p1;
 
-        
-
+     
         public TennisGame3(string player1Name, string player2Name)
         {
-            p1 = new Player(player1Name);
-            p2 = new Player(player2Name);
+            p1 = new ObservablePlayer(player1Name);
+            p2 = new ObservablePlayer(player2Name);
 
             ObserverTabellone tabellone = new ObserverTabellone();
-            p1.PlayerScored += tabellone.HandleEvent;
-            p2.PlayerScored += tabellone.HandleEvent;
+            p1.Player1Scored += tabellone.Player1ScoredOnePoint;
+            p2.Player2Scored += tabellone.Player2ScoredOnePoint;
             
         }
         
         public string GetScore()
         {
-            return rsc.GetScore(p1, p2);
+            //return rsc.GetScore(p1, p2);
+            return null;
         }
 
         public void WonPoint(string playerName)
         {
-            //if (playerName == "player1")
-            //    this.p1.AddPoint();
-            //else
-            //    this.p2.AddPoint();
+            if (playerName == "player1")
+                this.p1.Player1ScoredOnePoint();
+            else
+                this.p2.Player2ScoredOnePoint();
         }
 
     }
